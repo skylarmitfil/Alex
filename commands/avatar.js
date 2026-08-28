988cd3982dd776dbb3c8f96c7ba8a3e7a8aa6a3f
Skylar <skylarmitfil@gmail.com>
2026-08-28 20:01:58 +0000

    alias added to avatar

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,13 +1,14 @@
 module.exports = {
     name: 'avatar',
+    aliases: ['av'],
     async execute(message) {
-        // This will grab the user mentioned avatar,  or it'll default back to author
+        // Grab the mentioned user's avatar, or default to the author
         const user = message.mentions.users.first() || message.author;
-
-        // 2. This Will Fetch The Highest Resolution Image Link (including animated GIFs if applicable)
-        const avatarUrl = user.displayAvatarURL({ size: 1024, dynamic: true });
-
-        // 3. Reply back directly with the image link
+        
+        // Fetch the highest resolution image link (including animated GIFs)
+        const avatarUrl = user.displayAvatarURL({ size: 1024, dynamic: true }); 
+        
+        // Reply back directly with the image link
         await message.reply(`**${user.username}'s Avatar:**\n${avatarUrl}`);
     },
 };
