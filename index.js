41a4578308a810c752e9ee8bbe4e43e30edfabf5
Skylar <skylarmitfil@gmail.com>
2026-08-28 01:11:21 +0000

    Addition to complete the requirements

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
 
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once('ready', () => {
+    console.log(`${client.user.tag} is now ONLINE!`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
