c4ad120746fc0e29280e15c993869cc8a2a4c968
Skylar <skylarmitfil@gmail.com>
2026-08-28 10:58:27 -0500

    Create events/ready.js

---
+++ events/ready.js
@@ -0,0 +1,35 @@
+const { Events, ActivityType } = require('discord.js');
+
+module.exports = {
+    name: Events.ClientReady,
+    once: true, // This event only runs once when the bot boots
+    execute(readyClient) {
+        console.log(`${readyClient.user.tag} is now ONLINE!`);
+
+        // Define your array of multiple status messages
+        const statusOptions = [
+            { name: ' With Fire', type: ActivityType.Playing },
+            { name: 'To You', type: ActivityType.Listening },
+            { name: 'For My Commands', type: ActivityType.Watching },
+            { name: 'for bugs', type: ActivityType.Competing }
+        ];
+
+        let counter = 0;
+
+        // Set the initial presence immediately on boot
+        readyClient.user.setPresence({
+            activities: [statusOptions[0]],
+            status: 'dnd'
+        });
+
+        // Loop changes the status message every 15 seconds
+        setInterval(() => {
+            counter = (counter + 1) % statusOptions.length;
+            
+            readyClient.user.setPresence({
+                activities: [statusOptions[counter]],
+                status: 'dnd'
+            });
+        }, 15000);
+    },
+};
