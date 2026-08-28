c95d1a7bf306e364a69bfacb78db69fe02d23585
Skylar <skylarmitfil@gmail.com>
2026-08-28 01:23:55 +0000

    repair

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ client.once(Events.ClientReady, (readyClient) => {
 client.on(Events.MessageCreate, async (message) => {
     if (message.author.bot || !message.content.startsWith(PREFIX)) return;
 
-    const args = message.content.slice(PREFIX.length).trim().split(/+/);
+const args = message.content.slice(PREFIX.length).trim().split(/ +/);
     const commandName = args.shift().toLowerCase();
 
     // Dynamically grab the command from our collection
